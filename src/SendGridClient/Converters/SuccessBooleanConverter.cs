3de64cadb74c15b14bf29c7ace0b06d7f3b74062
Alex Forbes-Reed <alexreed@outlook.com>
2016-05-02 17:53:12 +0100

    added custom converter for sendgrid "success" response

---
+++ src/SendGridClient/Converters/SuccessBooleanConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace SendGrid.Client.Converters
+{
+	public class SuccessBoolConverter : JsonConverter
+	{
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			throw new NotSupportedException();
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			return reader.Value.ToString() == "success";
+		}
+
+		public override bool CanConvert(Type objectType)
+		{
+			return objectType == typeof(bool);
+		}
+	}
+}
--- src/SendGridClient/Models/GenericResponse.cs
+++ src/SendGridClient/Models/GenericResponse.cs
@@ -1,10 +1,12 @@
 ﻿using Newtonsoft.Json;
+using SendGrid.Client.Converters;
 
 namespace SendGrid.Models
 {
 	public class GenericResponse
 	{
 		[JsonProperty("message")]
+		[JsonConverter(typeof(SuccessBoolConverter))]
 		public bool Message { get; set; }
 	}
 }
